76fe5a35a8c3bcf1d00fd88e0931f82fd69b5387
cm-rwakatsuki <57384023+cm-rwakatsuki@users.noreply.github.com>
2023-08-15 00:38:09 +0900

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-const ACCESS_KEY = 'AKIAQA2IFJE3PKLGSH4Z';
-
 app.get('/', (req, res) => {
   const userComment = req.query.comment || '';
   // res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
